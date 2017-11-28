6e4ae4b7590cf3ad2e5d36e1d16b9c1040d966cc
NxtLife\Pankaj <kharetwal.pankaj111@gmail.com>
2017-11-28 12:20:47 +0530

    created 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
