ea3509e96952fa754288977b357b6bab97d3fbd7
NxtLife\Pankaj <kharetwal.pankaj111@gmail.com>
2017-11-28 16:17:18 +0530

    created routing.module.ts and updated other files by imcuding modules

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
