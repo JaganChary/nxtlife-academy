7e3b7bf5c6d5847f042c605443a90a373c087938
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-13 10:24:51 +0530

    imported department and employee form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule, ValidatorFn } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+ 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -15,6 +17,10 @@ import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './shared/auth-guard.service';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
@@ -24,16 +30,20 @@ import { RegisterComponent } from './register/register.component';
     FooterComponent,
     ContactComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    EmployeeFormComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
