7de66f0c5a715e9900dae8f18fd1062facff04c3
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-13 10:19:45 +0530

    added the links to Add employee form and departments to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,48 @@ import { Router } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './shared/auth-guard.service';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { DepartmentComponent } from './department/department.component';
+ 
 const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full'  },
-  { path: 'register', component: RegisterComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'home', component: HomeComponent }
+  { 
+    path: 'register', 
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/login', 
+    pathMatch: 'full',
+  },
+  { 
+    path: 'login',
+    component: LoginComponent, 
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent,
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'contact', 
+    component: ContactComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'employee-form',
+    component: EmployeeFormComponent
+  },
+  {
+    path: 'department',
+    component: DepartmentComponent
+  },
+  {
+    path: '**',
+    component: LoginComponent
+  } 
+  
 ]
 
 @NgModule({
