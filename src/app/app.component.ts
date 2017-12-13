05234e9612ac4be92a0b0293add867e4b8c461d6
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-13 10:35:16 +0530

    Made basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ContactComponent } from './contact/contact.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 import { Router } from '@angular/router'; 
 
