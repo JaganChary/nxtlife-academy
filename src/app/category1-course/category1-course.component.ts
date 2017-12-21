fd88a2598d790f19ebd86f84d7fd53c90a06946a
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-21 11:13:06 +0530

    added a variable x for interpolation in the html file

--- src/app/category1-course/category1-course.component.ts
+++ src/app/category1-course/category1-course.component.ts
@@ -12,6 +12,8 @@ import { CommonHttpService } from '../shared/commonHttp.service';
 export class Category1CourseComponent implements OnInit {
   courses: any;
   categoryData: any;
+
+  x:string;
   
   constructor(
     private router: Router,
@@ -23,20 +25,24 @@ export class Category1CourseComponent implements OnInit {
   ngOnInit() {
 
     if(this.traverseService.categoriesData == null || undefined) {
+      console.log("request  sent");
       this.commonHttpService.getCategories()
       .subscribe((res: any) => {
         this.traverseService.storeCategoriesData(res);
         const id = +this.route.snapshot.paramMap.get('id');
         this.categoryData = this.traverseService.getCategoryDataById(id);
         this.courses = this.categoryData.courses;
+        this.x = `category/${this.categoryData.courseCategoryId}/courses/chapters`;
       }, (error: any) => {
         console.log(error);
       
       })
     } else {
+      console.log("request not sent");
         const id = +this.route.snapshot.paramMap.get('id');
         this.categoryData = this.traverseService.getCategoryDataById(id);
         console.log(this.categoryData);
+        this.x = `category/${this.categoryData.courseCategoryId}/courses/chapters`;
         this.courses = this.categoryData.courses;
     }
   }
