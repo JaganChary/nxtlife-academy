80546f61f46d0a472e2804be3f445f5f78f5ae75
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-25 17:37:26 +0530

    changed the sidebar to a simple one

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonHttpService } from '../../shared/commonHttp.service';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(
+    private commonHttpService: CommonHttpService  
+  ) { }
 
   ngOnInit() {
+    
+  }
+
+  // Getting all Subscriptions List
+  btnSubscriptions() {
+    this.commonHttpService.getSubscriptions()
+    .subscribe((res: any) => {
+      console.log(res);
+    }, (error: any) => {
+
+    });
+  }
+
+  // Getting all Employees List
+  btnEmployees() {
+    this.commonHttpService.getCategories()
+    .subscribe((res: any) => {
+      console.log(res);
+    }, (error: any) => {
+      console.log(error);  
+    })
+  }
+
+  // Getting all  Departments List
+  btnDepartments() {
+    this.commonHttpService.getDepartments()
+    .subscribe((res: any) => {
+      console.log(res);
+    }, (error: any) => {
+      console.log(error);  
+    })
   }
 
 }
