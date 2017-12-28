a31e9f5eac26091f2d9d24facff4a69f524b1066
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-28 16:37:50 +0530

    wrote a method to store course data to access it through Id

--- src/app/shared/traverse.service.ts
+++ src/app/shared/traverse.service.ts
@@ -5,21 +5,34 @@ import { log } from 'util';
 export class TraverseService {
   response: Array<any>;
   categoriesData: object;
+  coursesData: object;
 
   constructor() { }
+
+  // *********** Storing categories Data *********** //
 
   storeCategoriesData(categories: Array<any>) {
 
     this.categoriesData = {};
     
+    this.coursesData = {};
+
     categories.forEach((catg:any)=>{
-        this.categoriesData[catg.courseCategoryId]=catg;
+        this.categoriesData[catg.courseCategoryId] = catg;
+
+        catg.courses.forEach((course: any) => {
+          this.coursesData[course.courseId] = course;
+        });
+
     });
   }
 
   getCategoryDataById(id: number) {
     return this.categoriesData[id];
   }
-}
 
+  getCourseDataById(id: number) {
+    return this.coursesData[id];
+  }
+}
 
