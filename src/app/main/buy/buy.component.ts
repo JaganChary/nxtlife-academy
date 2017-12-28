50cf5abf972bc620d6d64fb182733b530a9ddeef
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-28 18:36:03 +0530

    creatd buy component,still incomplte functionality

--- src/app/main/buy/buy.component.ts
+++ src/app/main/buy/buy.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonHttpService } from '../../shared/commonHttp.service';
+import { TraverseService } from '../../shared/traverse.service';  
+import { Router, ParamMap, ActivatedRoute } from '@angular/router';
+import { ActionSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-buy',
@@ -7,19 +10,66 @@ import { CommonHttpService } from '../../shared/commonHttp.service';
   styleUrls: ['./buy.component.css']
 })
 export class BuyComponent implements OnInit {
+  courses: any;
+  categoryData: any;
+  categoriesData: any;
+  coursesData: any;
+  inputNumber: number;
+  x: number;
 
   constructor(
-    private commonHttpService: CommonHttpService
+    private route: ActivatedRoute,
+    private commonHttpService: CommonHttpService,
+    private traverseService: TraverseService
   ) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    if(this.traverseService.categoriesData == null || undefined) {
+
+      this.commonHttpService.getCategories()
+      .subscribe((res: any) => {
+        this.traverseService.storeCategoriesData(res);
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.coursesData = this.traverseService.getCourseDataById(id);
+        console.log(this.coursesData);
+       
+      }, (error: any) => {
+        console.log(error);
+      
+      })
+    } else {
+
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.coursesData = this.traverseService.getCourseDataById(id);
+        console.log(this.coursesData);
+      }
   }
-  btnClick() {
-    this.commonHttpService.postSubscription()
-    .subscribe((res: any) => {
-      console.log('New subscription: ', res);
-    },(error: any) => {
-      console.log(error);
-    })
+
+
+  modelChanged() {
+    console.log(typeof(this.inputNumber));
+    console.log(this.inputNumber);
+
+    if((this.inputNumber < 1) || (this.inputNumber == null || undefined)) {
+      this.inputNumber = 1;
+
+    } 
+
   }
+  
+
+
+
+
+
+
+  // ******** Post Request sent on Button Click ******** //
+  // btnClick() {
+  //   this.commonHttpService.postSubscription()
+  //   .subscribe((res: any) => {
+  //     console.log('New subscription: ', res);
+  //   },(error: any) => {
+  //     console.log(error);
+  //   })
+  // }
 }
