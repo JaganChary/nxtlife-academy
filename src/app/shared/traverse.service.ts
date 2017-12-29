b3a6c94b70a48eaa323dc69bd4545538d78c750a
Jagan Chary <jagannadhachary7182@gmail.com>
2017-12-29 17:00:33 +0530

    added methods to add,remove and retrive cart items

--- src/app/shared/traverse.service.ts
+++ src/app/shared/traverse.service.ts
@@ -3,9 +3,11 @@ import { log } from 'util';
 
 @Injectable()
 export class TraverseService {
+  cartItems: number;
+  cartData: any[];
   response: Array<any>;
   categoriesData: object;
-  coursesData: object;
+  coursesData: Object;
 
   constructor() { }
 
@@ -20,19 +22,48 @@ export class TraverseService {
     categories.forEach((catg:any)=>{
         this.categoriesData[catg.courseCategoryId] = catg;
 
+        // *********** Storing course Data *********** //
+        
         catg.courses.forEach((course: any) => {
           this.coursesData[course.courseId] = course;
+          console.log(this.coursesData);
         });
-
+        
     });
   }
 
+  
+  // *******  Retrieving CategoryDataById ******* //
   getCategoryDataById(id: number) {
     return this.categoriesData[id];
   }
 
+  // *******  Retrieving CourseDataById ******* //
   getCourseDataById(id: number) {
     return this.coursesData[id];
   }
+
+  // *******  Adding CartData ******* //
+  addCartData() {
+    this.cartData.push(this.coursesData);
+    console.log(this.cartData);
+    this.cartItems = this.cartData.length;
+    console.log(this.cartItems);
+  }
+
+  // *******  Retrieving CartData ******* //
+  getCartData() {
+    console.log(this.cartData);
+    return this.cartData;
+  }
+
+  // *******  Removing CartData ******* //
+  removeCartData() {
+    this.cartData.splice(this.cartData.indexOf(this.coursesData), 1);
+    console.log(this.cartData);
+  }
+
+  
+
 }
 
