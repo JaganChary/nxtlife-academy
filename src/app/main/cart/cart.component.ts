ffdcfee7f0c99ecd4d1abe743f0e8401e2e7284f
JaganChary <jagannadhachary7182@gmail.com>
2018-01-02 10:24:06 +0530

    created cart component which shows the the items or courses in the cart and and provides all fucnctionalityD

--- src/app/main/cart/cart.component.ts
+++ src/app/main/cart/cart.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TraverseService } from '../../shared/traverse.service';
+import { CommonHttpService } from '../../shared/commonHttp.service';
+import { CartValueService } from '../../shared/cart-value.service';
+import {
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+  ValidatorFn,
+  FormsModule,
+  FormGroup,
+  FormArray,
+  FormArrayName,
+  FormBuilder,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +22,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  i: number;
+  cartValue: number;
+  cartData: any;
+  inputNumber: Array<any> = [1];
+  courseId: any;
+  coursesData: any;
+  coursesSubscribed: object = {};
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private traverseService: TraverseService,
+    private commonHttpService: CommonHttpService,
+    private cartValueService: CartValueService
+  ) { }
 
   ngOnInit() {
+
+    this.cartData = this.cartValueService.getCartData();
   }
+
+  modelChanged() {
+
+    if((this.inputNumber[0] < 1) || (this.inputNumber == null || undefined)) {
+      console.log(this.inputNumber);
+      this.inputNumber[0] = 1;
+    } 
+  }
+
+  btnAdd() {
+    this.inputNumber = this.inputNumber[this.i] + 1;
+  }
+  
+  btnSubstract() {
+    this.inputNumber[this.i] = this.inputNumber[this.i] - 1;
+    if(this.inputNumber[this.i] < 1) {
+      this.inputNumber[this.i] = 1; 
+    }
+  }
+
+  // ********** Removig Cart Items ********** // 
+  btnRemove(course: any) {
+    this.cartValueService.removeCartData(course);
+    console.log(this.cartValueService.cartData.length);
+  }
+
+  
+  // Post Request Sent on BtnClick
+  // btnClick() {
+
+  // }
 
 }
