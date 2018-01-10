494d0728ce380eebfaf9643280974bfbbd57c052
JaganChary <jagannadhachary7182@gmail.com>
2018-01-10 18:39:11 +0530

    created admin module and shifted manager module inside main

---
+++ src/app/main/manager/manager.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-manager',
+  templateUrl: './manager.component.html',
+  styleUrls: ['./manager.component.css']
+})
+export class ManagerComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
