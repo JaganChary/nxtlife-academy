feea5370ca8192438e32f25bc5e9b9d49450cbb9
JaganChary <jagannadhachary7182@gmail.com>
2018-01-12 13:21:14 +0530

    added separate sidebar for manager

--- src/app/main/manager/manager.module.ts
+++ src/app/main/manager/manager.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManagerComponent } from './manager.component';
 import { ManagerService } from './manager.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ManagerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ManagerComponent
+      }
+    ])
   ],
   providers: [ManagerService]
   
