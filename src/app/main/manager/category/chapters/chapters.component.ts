5b11029407fb24cc25f6903b4050727976378f8d
JaganChary <jagannadhachary7182@gmail.com>
2018-01-15 12:54:06 +0530

    created chapters compoent, all logic done, need to receive array to show details

--- src/app/main/manager/category/chapters/chapters.component.ts
+++ src/app/main/manager/category/chapters/chapters.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-chapters',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chapters.component.css']
 })
 export class ChaptersComponent implements OnInit {
+  chapters: any;
+  courses: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit() {
+    // if (this.categoryService.categoriesData == null || this.categoryService.categoriesData == undefined) {
+    //   this.getDataFromServer();
+
+    // } else {
+    //   this.getDataDirectly();
+    // }
+
   }
+
+  // Retreive data from server
+
+  // getDataFromServer(): any {
+  //   this.categoryService.getManagerTasks()
+  //     .subscribe((res: any) => {
+  //       this.categoryService.storeCategoryData(res);
+  //       const id = +this.route.snapshot.paramMap.get('id');
+  //       this.courses = this.categoryService.getCourseDataByID(id);
+  //       this.chapters = this.courses.chapters;
+  //       console.log(this.chapters);
+  //     }, (err: any) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  // Retrieve data directly
+
+  // getDataDirectly(): any {
+  //   const id = +this.route.snapshot.paramMap.get('id');
+  //   this.courses = this.categoryService.getCourseDataByID(id);
+  //   this.chapters = this.courses.chapters;
+  // }
 
 }
