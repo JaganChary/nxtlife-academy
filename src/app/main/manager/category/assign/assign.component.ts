c8d2edd599d39011c5910b9028db666b948abe0d
JaganChary <jagannadhachary7182@gmail.com>
2018-01-16 17:41:25 +0530

    the assign compoennt now shows a list of assigned and uassigned employees and the manager can assign a new employee

--- src/app/main/manager/category/assign/assign.component.ts
+++ src/app/main/manager/category/assign/assign.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AssignService } from './assign.service';
 import { CategoryService } from '../category.service';
-import { ActivatedRoute } from '@angular/router/src/router_state';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-assign',
@@ -9,7 +9,12 @@ import { ActivatedRoute } from '@angular/router/src/router_state';
   styleUrls: ['./assign.component.css']
 })
 export class AssignComponent implements OnInit {
-  courseData: any;
+  courseId: any;
+  assignedEmployees: any;
+  unassigned: any[] = [];
+  assigned: any[] = [];
+  allEmployees: any;
+  courses: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -17,48 +22,116 @@ export class AssignComponent implements OnInit {
     private categoryService: CategoryService
   ) { }
 
-  ngOnInit( ) {
+  ngOnInit() {
 
-    if(!this.categoryService.coursesData ) {
+    if (!this.categoryService.coursesData) {
       this.getCoursesDataFromServer();
-      
+
     } else {
       this.getCoursesDataDirectly();
+      this.getList();
     }
-
-    this.getList();
   }
+
+  // Retrieving courses from server
 
   getCoursesDataFromServer(): any {
-    this.categoryService.getManagerTasks().
-    subscribe((res: any) => {
-      this.categoryService.storeCategoryData(res.data);
-      const id = +this.route.snapshot.paramMap.get('id');
-      this.courseData = this.categoryService.getCourseDataByID(id);
-      console.log(this.courseData);
+    this.categoryService.getManagerTasks()
+      .subscribe((res: any) => {
+        this.categoryService.storeCategoryData(res.data);
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.courses = this.categoryService.getCourseDataByID(id);
+        this.assignedEmployees = this.courses.assignedEmployees;
+        
+        this.getList();
 
-      this.getList();
-
-    }, (err: any) => {
-      console.log(err);
-    }) 
+      }, (err: any) => {
+        console.log(err);
+      })
   }
+
+  // Retrieving courses Directly
 
   getCoursesDataDirectly(): any {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.courseData = this.categoryService.getCourseDataByID(id);
-    console.log(this.courseData); 
+    this.courses = this.categoryService.getCourseDataByID(id);
+    this.assignedEmployees = this.courses.assignedEmployees;
   }
+
+  // All Employee List
 
   getList(): any {
     this.assignService.getAllEmployeeList()
-    .subscribe((res: any) => {
-      console.log(res);
-    }, (err: any) => {
-      console.log(err);
-    })
+      .subscribe((res: any) => {
+        this.allEmployees = res.data;
+        this.allEmployeeArray();
+
+      }, (err: any) => {
+        console.log(err);
+      })
   }
 
-  
+  allEmployeeArray(): any {
+    
+    if (this.assignedEmployees.length !== 0) {
+
+      this.allEmployees.forEach((element: any) => {
+        element.id
+        this.assignedEmployees.forEach((elem: any) => {
+          elem.assignedToEmployeeId
+          if (element.id == elem.assignedToEmployeeId) {
+            element['assignedEmployee'] = true;
+          } else {
+            element['checkValue'] = false;
+          }
+        })
+      })
+
+      for (let i = 0; i < this.allEmployees.length; i++) {
+        if (this.allEmployees[i].assignedEmployee) {
+          this.assigned.push(this.allEmployees[i]);
+
+        } else {
+          this.unassigned.push(this.allEmployees[i]);
+        }
+      }
+     
+
+    } else {
+
+      for (let j = 0; j < this.allEmployees.length; j++) {
+        this.allEmployees[j]['checkValue'] = false;
+        this.unassigned.push(this.allEmployees[j]);
+      }
+
+    }
+    // console.log('Unassigned', this.unassigned);
+    // console.log('Assigned', this.assigned);
+  }
+
+  // Button to assign course to employee  
+
+  btnClick(): any {
+    var arr = [];
+
+    for (let i = 0; i < this.allEmployees.length; i++) {
+      if (this.allEmployees[i].checkValue == true) {
+        arr.push(
+          {
+            courseId: this.courses.courseId,
+            employeeId: this.allEmployees[i].id
+          }
+        )
+      }
+    }
+
+    this.assignService.postEmployeeDetail(arr)
+      .subscribe((res: any) => {
+        console.log(res);
+      }, (err: any) => {
+        console.log(err);
+      })
+  }
+
 
 }
--- src/app/main/manager/category/assign/assign.service.ts
+++ src/app/main/manager/category/assign/assign.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CommonHttpService } from '../../../shared/commonHttp.service';
+import { debug } from 'util';
 
 @Injectable()
 export class AssignService {
@@ -10,5 +11,9 @@ export class AssignService {
 
   getAllEmployeeList(): any {
     return this.commonHttpService.get('/manager/employees?role=employee');
+  }
+
+  postEmployeeDetail(data): any {
+    return this.commonHttpService.post('/manager/assign/course', data);
   }
 }
