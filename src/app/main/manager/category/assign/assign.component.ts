cd1f7815d8cb0f9206f8a32b9412e72b2dc42061
JaganChary <jagannadhachary7182@gmail.com>
2018-01-16 10:05:02 +0530

    the html structure is complete , need to create object to asign

--- src/app/main/manager/category/assign/assign.component.ts
+++ src/app/main/manager/category/assign/assign.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AssignService } from './assign.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router/src/router_state';
 
 @Component({
   selector: 'app-assign',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assign.component.css']
 })
 export class AssignComponent implements OnInit {
+  courseData: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private assignService: AssignService,
+    private categoryService: CategoryService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit( ) {
+
+    if(!this.categoryService.coursesData ) {
+      this.getCoursesDataFromServer();
+      
+    } else {
+      this.getCoursesDataDirectly();
+    }
+
+    this.getList();
   }
+
+  getCoursesDataFromServer(): any {
+    this.categoryService.getManagerTasks().
+    subscribe((res: any) => {
+      this.categoryService.storeCategoryData(res.data);
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.courseData = this.categoryService.getCourseDataByID(id);
+      console.log(this.courseData);
+
+      this.getList();
+
+    }, (err: any) => {
+      console.log(err);
+    }) 
+  }
+
+  getCoursesDataDirectly(): any {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.courseData = this.categoryService.getCourseDataByID(id);
+    console.log(this.courseData); 
+  }
+
+  getList(): any {
+    this.assignService.getAllEmployeeList()
+    .subscribe((res: any) => {
+      console.log(res);
+    }, (err: any) => {
+      console.log(err);
+    })
+  }
+
+  
 
 }
--- src/app/main/manager/category/assign/assign.service.ts
+++ src/app/main/manager/category/assign/assign.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { CommonHttpService } from '../../../shared/commonHttp.service';
 
 @Injectable()
 export class AssignService {
 
-  constructor() { }
+  constructor(
+    private commonHttpService: CommonHttpService
+  ) { }
 
+  getAllEmployeeList(): any {
+    return this.commonHttpService.get('/manager/employees?role=employee');
+  }
 }
