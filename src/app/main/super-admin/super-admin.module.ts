ea71548b7e7e725c9473aebfc2419b4a110da0ee
JaganChary <jagannadhachary7182@gmail.com>
2018-01-17 17:27:52 +0530

    created a super admin module which will create categories, courses, topics, pages and templates

---
+++ src/app/main/super-admin/super-admin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SuperAdminComponent } from './super-admin.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SuperAdminComponent]
+})
+export class SuperAdminModule { }
