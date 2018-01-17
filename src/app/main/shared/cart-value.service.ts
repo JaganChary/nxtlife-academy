3fedab7cdfe69292dcecf02e7997364d6e516f0c
JaganChary <jagannadhachary7182@gmail.com>
2018-01-17 12:55:09 +0530

    cart value or cart items do not increase for identical item

--- src/app/main/shared/cart-value.service.ts
+++ src/app/main/shared/cart-value.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class CartValueService {
+  elem: any;
   cartItems: number;
   cartData: any[] = [];
   private currentCartValue = new Subject<number>();
@@ -10,28 +11,38 @@ export class CartValueService {
 
   constructor() { }
 
-  
+
   // *******  Adding CartData ******* //
   addCartData(course: any) {
-    
-    this.cartData.push(course);
-    // console.log(this.cartData);
-    
-    this.currentCartValue.next(this.cartData.length);
+
+    if (this.cartData) {
+
+      var y = this.cartData.findIndex(z => z.courseId == course.courseId);
+      console.log('Response', y);
+
+      if (y == -1) {
+        this.cartData.push(course);
+        console.log(this.cartData);
+        this.currentCartValue.next(this.cartData.length);
+        
+      } else {
+        console.log('Item already in Cart');
+      }
+    }
   }
 
   // *******  Retrieving CartData ******* //
   getCartData() {
-    
+
     // console.log(this.cartData);
     return this.cartData;
   }
 
   // *******  Removing CartData ******* //
   removeCartData(course: any) {
-    
+
     this.cartData.splice(this.cartData.indexOf(course), 1);
-    
+
     this.currentCartValue.next(this.cartData.length);
   }
 
