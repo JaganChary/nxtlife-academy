24c25cc212053c535415ec67eaee052c39f8025c
JaganChary <jagannadhachary7182@gmail.com>
2018-02-15 17:13:35 +0530

    created template four, need to add validations

--- src/app/main/categories/chapters/templates/template-four/template-four.component.ts
+++ src/app/main/categories/chapters/templates/template-four/template-four.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-template-four',
@@ -6,10 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-four.component.css']
 })
 export class TemplateFourComponent implements OnInit {
+  templateFourForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    this.initForm();
   }
 
+  initForm(): any {
+
+    this.templateFourForm = this.formBuilder.group({
+
+      heading: ['', Validators.required],
+
+      questions: this.formBuilder.array([
+        this.getQuestions()
+      ])
+
+    })
+  }
+
+  getQuestions(): any {
+    return this.formBuilder.group({
+
+      question: ['', Validators.required],
+
+      type: ['', Validators.required],
+
+      options: this.formBuilder.array([
+        this.getOptions()
+      ])
+    })
+  }
+
+  getOptions(): any {
+    return this.formBuilder.group({
+
+      answer: ['', Validators.required],
+
+      correct: ['', Validators.required],
+
+      description: ['', Validators.required]
+
+    })
+  }
+
+  addQuestions(): any {
+    
+  } 
+  
+  addOptions(): any {
+
+  }
+
+  deleteQuestions(): any {
+
+  }
+
+  deleteOptions(): any {
+    
+  }
+
+  
 }
